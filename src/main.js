daffa38e05ecd94f151c48b2800cd28e590840f9
Alexandre Calil Blasizza Paravani <alexandre.paravani@gruponewway.com.bt>
2023-08-14 18:03:27 -0300

    adição script start 2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const http = require('http');
 const httpProxy = require('http-proxy');
 
@@ -5,23 +6,24 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
 // Endereço IP que você deseja redirecionar
-const targetIP = process.env.ip;
+const targetIP = process.env.ip || '192.168.0.45';
 const targetPort = 3000; // A porta do servidor alvo
 
 // Criar o servidor proxy
 const proxyServer = http.createServer((req, res) => {
-  // Redirecionar todo o tráfego para o servidor alvo
-  proxy.web(req, res, { target: `http://${targetIP}:${targetPort}` });
+	// Redirecionar todo o tráfego para o servidor alvo
+	proxy.web(req, res, { target: `http://${targetIP}:${targetPort}` });
 });
 
 // Lidar com erros do proxy
 proxy.on('error', (err, req, res) => {
-  res.writeHead(500, { 'Content-Type': 'text/plain' });
-  res.end('Ocorreu um erro no proxy reverso.');
+	res.writeHead(500, { 'Content-Type': 'text/plain' });
+	res.end('Ocorreu um erro no proxy reverso.');
 });
 
 // Iniciar o servidor proxy
 const proxyPort = 8080; // A porta em que o proxy vai ouvir
 proxyServer.listen(proxyPort, () => {
-  console.log(`Servidor proxy reverso em execução na porta ${proxyPort}`);
+	console.log(`Servidor proxy reverso em execução na porta ${proxyPort}`);
+	console.log('ip', targetIP)
 });
